7adaf1f602ae1375d0eb577fdee498d5418254bf
Zaidu Abubakar <zaiduabubakar777@gmail.com>
2025-09-09 18:09:58 +0100

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import ProtectedRoute from './components/ProtectedRoute';
 import RegisterPage from './pages/RegisterPage';
@@ -7,20 +7,18 @@ import MiningHub from './pages/MiningHub';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="app-container">
-        <Routes>
-          {/* Public Routes */}
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/login" element={<LoginPage />} />
+    <div className="app-container">
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
 
-          {/* Protected Routes */}
-          <Route element={<ProtectedRoute />}>
-            <Route path="/" element={<MiningHub />} />
-          </Route>
-        </Routes>
-      </div>
-    </BrowserRouter>
+        {/* Protected Routes */}
+        <Route element={<ProtectedRoute />}>
+          <Route path="/" element={<MiningHub />} />
+        </Route>
+      </Routes>
+    </div>
   );
 }
 
