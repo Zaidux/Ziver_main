4a57d29ae464d3bf259f809a98f621b46e91284e
Zaidu Abubakar <zaiduabubakar777@gmail.com>
2025-09-09 14:53:22 +0100

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 // We will create these page components in the next steps
-// import RegisterPage from './pages/RegisterPage';
+import RegisterPage from './pages/RegisterPage'; // <-- IMPORT THIS
 // import LoginPage from './pages/LoginPage';
 // import MiningHub from './pages/MiningHub';
 
@@ -11,7 +11,7 @@ function App() {
       <div className="app-container">
         <Routes>
           {/* We'll uncomment these as we build them */}
-          {/* <Route path="/register" element={<RegisterPage />} /> */}
+          <Route path="/register" element={<RegisterPage />} /> {/* <-- UNCOMMENT THIS */}
           {/* <Route path="/login" element={<LoginPage />} /> */}
           {/* <Route path="/" element={<MiningHub />} /> */}
         </Routes>
