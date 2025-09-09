35fa907feaafa5404adff51f6fe36bbf3d970fa2
Zaidu Abubakar <zaiduabubakar777@gmail.com>
2025-09-09 23:30:35 +0100

    Update userRoutes.js

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
-// We will create this controller and middleware later
-const { getUserProfile, updateUserActivity } = require('../controllers/userController');
+const { 
+  getUserProfile, 
+  updateUserActivity,
+  recordHeartbeat // <-- NEW
+} = require('../controllers/userController');
 const { protect } = require('../middleware/authMiddleware');
 
 // @route   GET /api/user/me
-// @desc    Get logged in user's profile
-// @access  Private
 router.get('/me', protect, getUserProfile);
 
-
 // @route   POST /api/user/activity
-// @desc    Log a user activity to update their score
-// @access  Private
 router.post('/activity', protect, updateUserActivity);
 
+// @route   POST /api/user/heartbeat  <-- NEW ROUTE
+// @desc    Record that the user is currently active
+// @access  Private
+router.post('/heartbeat', protect, recordHeartbeat);
 
 module.exports = router;
-
