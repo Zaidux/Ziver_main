3a9a47570666c131e078ab250298fd76eb457a99
Zaidu Abubakar <zaiduabubakar777@gmail.com>
2025-09-09 20:33:44 +0100

    Update Layout.jsx

--- admin-panel/src/components/Layout.jsx
+++ admin-panel/src/components/Layout.jsx
@@ -1,1 +1,17 @@
+import React from 'react';
+import { NavLink, Outlet } from 'react-router-dom';
 
+const Layout = () => (
+  <div style={{ display: 'flex' }}>
+    <nav style={{ width: '200px', borderRight: '1px solid #333', padding: '1rem' }}>
+      <NavLink to="/">Dashboard</NavLink><br />
+      <NavLink to="/tasks">Tasks</NavLink><br />
+      <NavLink to="/settings">Settings</NavLink>
+    </nav>
+    <main style={{ flex: 1, padding: '1rem' }}>
+      <Outlet />
+    </main>
+  </div>
+);
+
+export default Layout;
