79598a0ae6343bd5a11174c07e128d7234c4222c
Zaidu Abubakar <zaiduabubakar777@gmail.com>
2025-09-09 20:59:55 +0100

    Update User.js

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -18,14 +18,14 @@ const User = {
     const { rows } = await db.query(query, [username, email, hashedPassword]);
     return rows[0];
   },
-  
+
   // Method to find a user by their ID
   findById: async (id) => {
-    const query = 'SELECT id, username, email, zp_balance, social_capital_score, created_at FROM users WHERE id = $1';
+    // THE FIX IS HERE: We've added "role" to the list of columns being selected.
+    const query = 'SELECT id, username, email, zp_balance, social_capital_score, created_at, role FROM users WHERE id = $1';
     const { rows } = await db.query(query, [id]);
     return rows[0];
   }
 };
 
 module.exports = User;
-
