3082b65e023e9facfaf98a51b7961ade4c74a8a4
Zaidu Abubakar <zaiduabubakar777@gmail.com>
2025-09-10 15:08:42 +0100

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,28 @@
 import { useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom'; // No BrowserRouter here
+import { Routes, Route } from 'react-router-dom';
 import { useAuth } from './context/AuthContext';
 import api from './services/api';
 
+// Layout and Component Imports
+import Layout from './components/Layout';
 import ProtectedRoute from './components/ProtectedRoute';
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
 import MiningHub from './pages/MiningHub';
+import TasksPage from './pages/TasksPage';
 
 function App() {
   const { user } = useAuth();
 
+  // Heartbeat effect (no changes here)
   useEffect(() => {
     let intervalId;
-
     if (user) {
       api.post('/user/heartbeat'); 
       intervalId = setInterval(() => {
         api.post('/user/heartbeat');
       }, 60000);
     }
-
     return () => {
       if (intervalId) {
         clearInterval(intervalId);
@@ -29,13 +31,19 @@ function App() {
   }, [user]);
 
   return (
-    // The BrowserRouter wrapper has been removed from this file
     <div className="app-container">
       <Routes>
+        {/* Public Routes */}
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
+
+        {/* Protected Routes */}
         <Route element={<ProtectedRoute />}>
-          <Route path="/" element={<MiningHub />} />
+          <Route element={<Layout />}> {/* <-- All pages inside here will have the Navbar */}
+            <Route path="/" element={<MiningHub />} />
+            <Route path="/tasks" element={<TasksPage />} />
+            <Route path="/referrals" element={<div>Referrals Page (Coming Soon)</div>} />
+          </Route>
         </Route>
       </Routes>
     </div>
