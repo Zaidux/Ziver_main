ccaa7b5ac6ea429521263b8ee2c2dd018b714a30
Zaidu Abubakar <zaiduabubakar777@gmail.com>
2025-10-11 19:26:28 +0100

    Update NotificationSettingsPage.jsx

--- client/src/pages/profile/pages/settings/NotificationSettingsPage.jsx
+++ client/src/pages/profile/pages/settings/NotificationSettingsPage.jsx
@@ -1,1 +1,8 @@
+import React from 'react';
+import NotificationSettings from '../../../../components/settings/NotificationSettings';
 
+const NotificationSettingsPage = () => {
+  return <NotificationSettings />;
+};
+
+export default NotificationSettingsPage;
