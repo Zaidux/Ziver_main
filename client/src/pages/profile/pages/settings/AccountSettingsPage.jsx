f1026e0307af300c2c396819efd1fae06617021d
Zaidu Abubakar <zaiduabubakar777@gmail.com>
2025-10-11 19:26:59 +0100

    Update AccountSettingsPage.jsx

--- client/src/pages/profile/pages/settings/AccountSettingsPage.jsx
+++ client/src/pages/profile/pages/settings/AccountSettingsPage.jsx
@@ -1,1 +1,8 @@
+import React from 'react';
+import AccountSettings from '../../../../components/settings/AccountSettings';
 
+const AccountSettingsPage = () => {
+  return <AccountSettings />;
+};
+
+export default AccountSettingsPage;
