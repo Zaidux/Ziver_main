12bf18c48afba8b074dbf65f592491e88375d393
Zaidu Abubakar <zaiduabubakar777@gmail.com>
2025-10-11 19:25:47 +0100

    Update SecuritySettingsPage.jsx

--- client/src/pages/profile/pages/settings/SecuritySettingsPage.jsx
+++ client/src/pages/profile/pages/settings/SecuritySettingsPage.jsx
@@ -1,1 +1,8 @@
+import React from 'react';
+import SecuritySettings from '../../../../components/settings/SecuritySettings';
 
+const SecuritySettingsPage = () => {
+  return <SecuritySettings />;
+};
+
+export default SecuritySettingsPage;
