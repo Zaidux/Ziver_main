697195d6f74990a9337de84c6f0f6e2865a8e895
Zaidu Abubakar <zaiduabubakar777@gmail.com>
2025-10-18 18:29:31 +0100

    Update walletRoutes.js

--- server/src/routes/walletRoutes.js
+++ server/src/routes/walletRoutes.js
@@ -5,6 +5,8 @@ const assetController = require('../controllers/features/wallet/assetController'
 const transactionController = require('../controllers/features/wallet/transactionController');
 const policyController = require('../controllers/features/wallet/policyController');
 const recoveryController = require('../controllers/features/wallet/recoveryController');
+const gasController = require('../controllers/features/wallet/gasController');
+const guardianController = require('../controllers/features/wallet/guardianController');
 
 const router = express.Router();
 
@@ -22,10 +24,21 @@ router.post('/simulate', protect, transactionController.simulateTransaction);
 router.post('/send', protect, transactionController.sendTransaction);
 router.get('/transaction-status', protect, transactionController.getTransactionStatus);
 
+// Gas Estimation
+router.post('/gas/estimate', protect, gasController.estimateFees);
+router.get('/gas/prices', protect, gasController.getGasPrices);
+router.get('/gas/token-prices', protect, gasController.getTokenPrices);
+
 // Spending Policies
 router.post('/policies', protect, policyController.createPolicy);
 router.get('/policies', protect, policyController.getPolicies);
 router.delete('/policies/:id', protect, policyController.deletePolicy);
+
+// Guardians Management
+router.post('/guardians', protect, guardianController.addGuardian);
+router.get('/guardians', protect, guardianController.getGuardians);
+router.put('/guardians/:id', protect, guardianController.updateGuardian);
+router.delete('/guardians/:id', protect, guardianController.removeGuardian);
 
 // Social Recovery
 router.post('/recovery/initiate', protect, recoveryController.initiateRecovery);
